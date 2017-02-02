26556241bc8745219062ed33c0b87544b45b4d8b
Yaroslav Kharchenko <undertext.2010@gmail.com>
2017-02-02 19:28:32 +0200

    Fix failing test.

--- src/test/java/unit/guitar/TuneInfoTest.java
+++ src/test/java/unit/guitar/TuneInfoTest.java
@@ -10,10 +10,10 @@ public class TuneInfoTest {
     @Test
     public void testStringRepresentation() {
         TuneInfo tuneInfo = new TuneInfo(GuitarNote.E2, 23);
-        Assert.assertEquals(tuneInfo.toString(), "E2 +23,00");
+        Assert.assertEquals(tuneInfo.toString(), "E2 +23.00");
         tuneInfo = new TuneInfo(GuitarNote.E2, 0.3456f);
-        Assert.assertEquals(tuneInfo.toString(), "E2 +0,35");
+        Assert.assertEquals(tuneInfo.toString(), "E2 +0.35");
         tuneInfo = new TuneInfo(GuitarNote.E2, -10);
-        Assert.assertEquals(tuneInfo.toString(), "E2 -10,00");
+        Assert.assertEquals(tuneInfo.toString(), "E2 -10.00");
     }
 }
